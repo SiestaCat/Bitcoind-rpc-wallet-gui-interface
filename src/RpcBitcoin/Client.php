2dcb12916d345bd1c6ff4d7870cba4556ac8fa8f
Gregorio <gregorio91@yahoo.co.uk>
2023-05-23 19:13:28 +0200

    remove files

--- src/RpcBitcoin/Client.php
+++ src/RpcBitcoin/Client.php
@@ -2,16 +2,72 @@
 
 namespace App\RpcBitcoin;
 
-class Client extends HttpClientcaller
+class Client
 {
 
-    public function __construct(string $rpc_username, string $rpc_password)
+    const SOCK_TIMEOUT = 2;
+
+    const JSON_RPC = '1.0';
+
+    public function __construct(private string $rpc_hostname, private int $rpc_port, private string $rpc_username, private string $rpc_password, private bool $rpc_is_https = false)
     {
-        parent::__construct($rpc_username, $rpc_password);
+        
+    }
+
+    public function listwallets():array|bool
+    {
+        $result = $this->call('listwallets');
+
+        return is_array($result) ? $result : false;
     }
 
     public function call(string $method, array $params = []):mixed
     {
         return $this->make_curl_call($method, $params);
+    }
+
+    /**
+     * 
+     * @param string $method 
+     * @param array $params 
+     * @return string|array|bool|null
+     */
+    protected function make_curl_call(string $method, array $params = []):mixed
+    {
+        $ch = curl_init("http://127.0.0.1:8332/");
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: text/plain',
+            'Authorization: Basic ' . base64_encode($this->rpc_username . ':' . $this->rpc_password)
+        ]);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($this->generateJson($method, $params)));
+
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+
+        $json_decoded = json_decode($response);
+
+        if(!is_object($json_decoded)) throw new \Exception('Unable to decode JSON result. Result: ' . $response);
+
+        return $json_decoded->result;
+    }
+
+    private function generateJson(string $method, array $params = []):array
+    {
+        $json = 
+        [
+            'jsonrpc' => self::JSON_RPC,
+            'id' => '$curltest',
+            'method' => $method,
+            'params' => $params
+        ];
+
+        return $json;
     }
 }
